03809b24a71ae8b2043454644c7aa378e4b8090a
AbramovaOlga <81623226+AbramovaOlga@users.noreply.github.com>
2022-03-29 14:29:46 +0900

    adding account activity

--- main/java/com/example/lt/MainActivity.java
+++ main/java/com/example/lt/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void activityOperator(View view) {
+        switch (view.getId()) {
+            case R.id.accountImg:
+                Intent intent0 = new Intent(this, accountActivity.class);
+                startActivity(intent0);
+                break;
+        }
     }
 }
