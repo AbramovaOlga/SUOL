eb7a3beec0886e382bec507d226392e2d5362da4
AbramovaOlga <81623226+AbramovaOlga@users.noreply.github.com>
2022-03-29 16:44:06 +0900

    help page

---
+++ main/java/com/example/lt/answersActivity.java
@@ -0,0 +1,26 @@
+package com.example.lt;
+
+import static com.example.lt.helpActivity.answersCount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class answersActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answers);
+
+        switch(answersCount){
+            case 0:
+                setContentView(R.layout.activity_answers); break;
+            case 1:
+                setContentView(R.layout.h_second_answer); break;
+        }
+    }
+}
--- main/java/com/example/lt/helpActivity.java
+++ main/java/com/example/lt/helpActivity.java
@@ -2,13 +2,36 @@ package com.example.lt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class helpActivity extends AppCompatActivity {
+
+    public static int answersCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        String[] array = getResources().getStringArray(R.array.helpPage);
+        ListView listView = findViewById(R.id.helpList);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    answersCount = i;
+                    Intent intent0 = new Intent(getApplicationContext(), answersActivity.class);
+                    startActivity(intent0);
+
+            }
+        });
     }
+
+
+
 }
