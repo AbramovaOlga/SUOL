2f3011b4848fa9f2d222d7826b80548ff69e2674
AbramovaOlga <81623226+AbramovaOlga@users.noreply.github.com>
2022-03-31 06:48:58 +0900

    login page

---
+++ main/java/com/example/lt/loginActivity.java
@@ -0,0 +1,28 @@
+package com.example.lt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onSignUpClick(View view){
+        setContentView(R.layout.sign_up);
+    }
+    public void onLoginClick(View view){
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginBtnClick(View view){
+        Intent intent2 = new Intent(this, MainActivity.class);
+        startActivity(intent2);
+    }
+}
