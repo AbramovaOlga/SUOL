b73200259d02ad08265882c40103aaabb3316fae
AbramovaOlga <81623226+AbramovaOlga@users.noreply.github.com>
2022-03-31 04:46:44 +0900

    account page

--- main/java/com/example/lt/accountActivity.java
+++ main/java/com/example/lt/accountActivity.java
@@ -2,7 +2,9 @@ package com.example.lt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class accountActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class accountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
     }
+
+
+
+    public void activityOperator(View view) {
+        Intent intent2 = new Intent(this, helpActivity.class);
+        startActivity(intent2);
+
+    }
+
 }
