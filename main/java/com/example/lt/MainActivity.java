22b7821974b26fda9c3a5d667e94016f680a3dd6
AbramovaOlga <81623226+AbramovaOlga@users.noreply.github.com>
2022-03-30 23:12:56 +0900

    Revert "Revert "main activity scrollView""
    This reverts commit 4ff5b69b86927a1dda6a00f1b856eaa2c16ffca1.

--- main/java/com/example/lt/MainActivity.java
+++ main/java/com/example/lt/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.lt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +32,14 @@ public class MainActivity extends AppCompatActivity {
         backPressed = System.currentTimeMillis();
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 
     public void activityOperator(View view) {
         switch (view.getId()) {
@@ -57,4 +67,7 @@ public class MainActivity extends AppCompatActivity {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
     }
+
+
+
 }
